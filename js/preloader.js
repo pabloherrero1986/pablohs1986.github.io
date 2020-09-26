47d7e1fb6a3a834356ecf992def48cb5f7f4cb29
pablohs1986 <pabloherrero1986@gmail.com>
2020-09-26 18:27:14 +0200

    Delete old files. Creating template for new one.

--- js/preloader.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-preloaderFadeOutTime = 250;
-function hidePreloader() {
-var preloader = $('.spinner-wrapper').delay(2500);
-preloader.fadeOut(preloaderFadeOutTime);
-}
-hidePreloader();
-});
