ba11ff8a9d302742a33e1dd95baf96ff65d4eb33
pablohs1986 <pabloherrero1986@gmail.com>
2020-10-02 23:38:35 +0200

    Scrollspy ended

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
+// Bootstrap scrollspy
 
+$(document).ready(function(){
+    $('body').scrollspy({target: ".navbar", offset: 70});   
+  });
+
+// Bootstrap smooth scrolling
+$("#navbar a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 300, function(){
+        window.location.hash = hash;
+      });
+    }
+});
 
 // // UI controller
 // var activeSection, id;
