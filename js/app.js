8af7a81a29c9d4e782d885f30c3b20df757c1495
pHerrero <pherrero@MacBook-Air-de-Pablo.local>
2020-10-17 20:09:27 +0200

    Added thanks page

--- js/app.js
+++ js/app.js
@@ -111,3 +111,24 @@ $(window).scroll(function() {
     });
   }
 });
+
+$(function(){
+	$(".typedFormAnswer").typed({
+		strings: [
+      "Thank you. <br/>Your message has been sent. <br/>I will contact you soon :)"],
+		typeSpeed: 70,
+    startDelay: 0,
+    loop: true,
+    shuffle: true,
+    showCursor: true,
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
